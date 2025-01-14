36c6bb32de60d3cd407cd199408ce45d8b849193
OladipoOye <dipooluwa17@gmail.com>
2025-01-14 01:09:50 +0000

    quiz app init

---
+++ Next_js/quiz_app/src/app/(quizzes)/French/page.js
@@ -0,0 +1,24 @@
+import Quiz from "@/app/components/quiz";
+
+export default function French() {
+    const french = [
+        {fr: 'sauter', en: 'hello'}, 
+        {fr: 'aller', en: 'hello'}, 
+        {fr: 'sauveguarder', en: 'hello'}, 
+        {fr: 'lever', en: 'hello'}, 
+        {fr: 'guerir', en: 'hello'}, 
+        {fr: 'subventionner', en: 'hello'}, 
+        {fr: 'accueillir', en: 'hello'}, 
+        {fr: 'prendre', en: 'hello'}, 
+        {fr: 'mettre', en: 'hello'}, 
+        {fr: 'courrir', en: 'hello'}, 
+        {fr: 'aggrandir', en: 'hello'}, 
+        {fr: 'ameliorer', en: 'hello'}, 
+        {fr: 'se sentir', en: 'hello'}, 
+        {fr: 'tirer', en: 'hello'}, 
+        {fr: 'criore', en: 'hello'}, 
+    ]
+    return (
+        <Quiz name="French" q={french} />
+    );
+}
---
+++ Next_js/quiz_app/src/app/components/quiz.js
@@ -0,0 +1,79 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+
+export default function Quiz(props) {
+    const [input, setInput] = useState(''); //for the user input values
+    const [current, setCurrent] = useState(0); // for the current level
+    const [streak, setStreak] = useState(0); // for the streak
+    const [error, setError] = useState(false);
+    const [maxStreak, setMaxStreak] = useState(0);
+    
+    const list = props.q;
+    const name = props.name;
+    
+    //for the random questions
+    const randomize = () => {
+        const rIndex = Math.floor(Math.random() * list.length);
+        setCurrent(rIndex);
+    }
+    
+    //for the imputs
+    const handleChange = evt => {
+        setInput(evt.target.value);
+    }
+    
+    
+    // for the question handling
+    const handleSubmit = evt => {
+        evt.preventDefault();
+
+        if (input.toLowerCase() === list[current][1]) {
+            setStreak((prev) => prev + 1);
+            setError(false);
+
+            localStorage.setItem('streak', streak + 1);
+            localStorage.setItem('maxStreak', Math.max(streak, maxStreak));
+        } else {
+            setError(`wrong, the correct answer for ${list[current][0]} is ${list[current][1]} `);
+            setStreak(0);
+
+            localStorage.setItem('streak', 0);
+
+        }
+
+        setInput('');
+        randomize();
+    }
+    
+    useEffect( () => {
+        randomize();
+        setStreak(parseInt(localStorage.getItem('streak')) || 0);
+        setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0);
+    }, [])
+    
+    return (
+        <div>
+            <header>
+                <h1> {name} Quiz</h1>
+                <div>
+                    <p> {streak} / {maxStreak} </p>
+                </div>
+            </header>
+            
+            <div>
+                {list[current][0]}
+            </div>
+            
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={handleChange} />
+            </form>
+            
+            {error && <p>{error}</p>}
+        </div>
+    );
+}
