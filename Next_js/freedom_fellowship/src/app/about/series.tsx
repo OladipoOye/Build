db97e4403a31a9e2af674f50e66119bf37bda685
OladipoOye <dipooluwa17@gmail.com>
2025-01-22 09:35:10 +0000

    replacing errors

--- Next_js/freedom_fellowship/src/app/about/series.tsx
+++
@@ -1,7 +0,0 @@
-export default function Series() {
-    //explore the chakra tabs section, see what works
-    //create tabs, with the content leading to iframes with appropriate aspect ratio
-    return (
-        <></>
-    );
-};
