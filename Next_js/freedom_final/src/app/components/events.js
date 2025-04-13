7fbadf3dc3584a00f3a10e18eeb33d68be4ed6bb
OladipoOye <dipooluwa17@gmail.com>
2025-04-13 20:07:52 +0100

    updates

--- Next_js/freedom_final/src/app/components/events.js
+++ Next_js/freedom_final/src/app/components/events.js
@@ -1,28 +1,53 @@
 "use client"
 import { Box, HStack } from "@chakra-ui/react";
 import { chakra } from "@chakra-ui/react";
+import { useEffect } from "react";
+
+function hov() {
+    //hover function for the event boxes
+    const box = document.querySelectorAll(".box");
+    box.forEach((b) => {
+        b.addEventListener("mouseover", () => {
+            b.style.backgroundColor = "white";
+            b.style.zIndex = "10";
+            b.style.transition = "0.5s";
+            b.style.transform = "scale(1.4)";
+            b.style.color = "black";
+        });
+        b.addEventListener("mouseout", () => {
+            b.style.backgroundColor = "black";
+            b.style.zIndex = "1";
+            b.style.transition = "0.5s";
+            b.style.transform = "scale(1)";
+            b.style.color = "white";
+        });
+    });
+}
 
 export default function Events() {
+    useEffect(() => {
+        hov(); // Call the hover function after the component mounts
+    }, []);
     //event boxes, lined horizontally with images and the descriptions
     return (
-            <Box bg="white" w="100%" h="60vh">
+            <Box bg="white" w="100%" h="80vh">
                     <chakra.h2 textAlign='center' color="black" textDecoration='underline' fontSize='2xl' > Events </chakra.h2>
                     <HStack ml="2vw" mt="4vh" gap="2vw" w='100%' >
                         
-                        <Box w="30%" border=" 2px dashed black" borderRadius="2em" bg="black" h='50vh' >  
-                            <chakra.h3 textDecoration="underline" textAlign='center' color='white' fontWeight='bold' > Event x</chakra.h3> 
+                        <Box className="box" color="white" w="30%" borderRadius="2em" bg="black" h='50vh'>  
+                            <chakra.h3 textDecoration="underline" textAlign='center' fontWeight='bold' > Event x</chakra.h3> 
                             <chakra.img src='#' h="20%" w="80%" alt='event x' />
-                            <chakra.p color='white'> Event x is the x event of the year<chakra.br/> covering y and z </chakra.p> 
+                            <chakra.p p="8%"> Event x is the x event of the year<chakra.br/> covering y and z </chakra.p> 
                         </Box>
-                        <Box w="30%" border=" 2px dashed black" borderRadius="2em" bg="black" h='50vh' > 
-                            <chakra.h3 textDecoration="underline" textAlign='center' color='white' fontWeight='bold' > Event y</chakra.h3>
+                        <Box className="box" color="white" w="30%" borderRadius="2em" bg="black" h='50vh' > 
+                            <chakra.h3 textDecoration="underline" textAlign='center' fontWeight='bold' > Event y</chakra.h3>
                             <chakra.img src='#' h="20%" w="80%" alt='event y' /> 
-                            <chakra.p>Event y is the y event of the year<chakra.br/> covering something and something else </chakra.p> 
+                            <chakra.p p="8%">Event y is the y event of the year<chakra.br/> covering something and something else </chakra.p> 
                         </Box>
-                        <Box w="30%" border=" 2px dashed black" borderRadius="2em" bg="black" h='50vh' >
-                            <chakra.h3 textDecoration="underline" textAlign='center' color='white' fontWeight='bold' > Event z</chakra.h3> 
+                        <Box className="box" color="white" w="30%" borderRadius="2em" bg="black" h='50vh' >
+                            <chakra.h3 textDecoration="underline" textAlign='center' fontWeight='bold' > Event z</chakra.h3> 
                             <chakra.img src='#' h="20%" w="80%" alt='event z' />
-                            <chakra.p> Event z is the z event of the year<chakra.br/> allowing for x and z to happen also </chakra.p> 
+                            <chakra.p p="8%"> Event z is the z event of the year<chakra.br/> allowing for x and z to happen also </chakra.p> 
                         </Box>
                     
                     </HStack>
