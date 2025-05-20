b65f251af2d8060e4ebdd0ffec6f2c2036f7d3fe
OladipoOye <dipooluwa17@gmail.com>
2025-05-20 17:33:27 +0100

    pendulum fixed

--- numerical_code/Graph plotting/Mechanics/pend_simul.py
+++ numerical_code/Graph plotting/Mechanics/pend_simul.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# eom is thetaddot = (g/l)*sin(theta)
+# eom is thetaddot = -(g/l)*sin(theta)
 # theta is the angle of the pendulum
 # theta0 is the initial angle
 # thetadot is the angular velocity
@@ -19,7 +19,7 @@ def pendulum(t, theta0, thetadot0, N):
     thetaddot = np.zeros(N)
     theta[0] = theta0
     thetadot[0] = thetadot0
-    thetaddot[0] = (g/l) * np.sin(theta0)
+    thetaddot[0] = -(g/l) * np.sin(theta0)
     
     #verlet second order method
     theta_v = np.zeros(N)
@@ -27,16 +27,16 @@ def pendulum(t, theta0, thetadot0, N):
     thetaddot_v = np.zeros(N)
     theta_v[0] = theta0
     thetadot_v[0] = thetadot0
-    thetaddot_v[0] = (g/l) * np.sin(theta0)
+    thetaddot_v[0] = -(g/l) * np.sin(theta0)
     
     
     for n in range(1, N):
         #accelerations
-        thetaddot[n] = (g/l) * np.sin(theta[n-1])
-        thetaddot_v[n] = (g/l) * np.sin(theta_v[n-1]) 
+        thetaddot[n] = -(g/l) * np.sin(theta[n-1])
+        thetaddot_v[n] = -(g/l) * np.sin(theta_v[n-1]) 
         
         #velocities
-        thetadot[n] = thetaddot[n-1] + dt*thetaddot[n]
+        thetadot[n] = thetadot[n-1] + dt*thetaddot[n]
         thetadot_v[n] = thetadot_v[n-1] + 0.5*(thetaddot_v[n-1] + thetaddot_v[n])*dt
         
         #displacements
@@ -46,7 +46,10 @@ def pendulum(t, theta0, thetadot0, N):
     t_list = np.linspace(0, t, N)
     plt.plot(t_list, theta, 'r')
     plt.plot(t_list, theta_v, 'g')
+    plt.ylabel("amplitude")
+    plt.xlabel("time")
+    plt.legend(loc=1)
     plt.title('angular diplacement against time')
     plt.show()
     
-pendulum(10, np.pi/4, 0, 10000)
+pendulum(20, np.pi/4, 0, 300000)
